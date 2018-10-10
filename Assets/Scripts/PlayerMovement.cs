f5ae74948190131d8cd096cd59bf677824152205
Fredrik Ehrs <fredrik.ehrs@elev.ga.lbs.se>
2018-10-10 13:54:06 +0200

    FredrikCommit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public TriggerScript Hitbox1;
+    public TriggerScript Hitbox2;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W) && Hitbox1.TriggerOn == false)
+        {
+            transform.Translate(0f, speed, 0f, Space.World);
+        }
+        else if (Input.GetKey(KeyCode.S) && Hitbox2.TriggerOn == false)
+        {
+            transform.Translate(0f, -speed, 0f, Space.World);
+        }
+        
+    }
+}
