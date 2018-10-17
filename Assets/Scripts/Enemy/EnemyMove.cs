ddeffc90527f97313c82b546aeb18a474a84558a
Fredrik Ehrs <fredrik.ehrs@elev.ga.lbs.se>
2018-10-17 15:46:47 +0200

    Enemy Movement Fixad

--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     public float moveSpeed;
+    public Transform fransTorm;
     // Use this for initialization
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         transform.Translate(-moveSpeed, 0f, 0f, Space.World);
     }
 }
