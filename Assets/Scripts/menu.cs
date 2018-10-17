f94bd05e2252e0798c478f38b907157a4c33af48
Hampus Hernstig <hampus.hartmanhernstig@elev.ga.lbs.se>
2018-10-17 14:47:47 +0200

    Menyer

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene("Main");
+
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
