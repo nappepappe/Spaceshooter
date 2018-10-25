73e022e94bf75554457b3ba59a632f5a68291e4c
Fredrik Ehrs <fredrik.ehrs@elev.ga.lbs.se>
2018-10-25 14:57:20 +0200

    Fixad start och musik
    Gjort så att man inte kan göra något förrens tre sekunder, har också lagt till musik som blir tyst när illuminati kommer in på skärmen.

---
+++ Assets/Scripts/Misc/MusicyBoi.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicyBoi : MonoBehaviour
+{
+    public AudioSource musicSource;
+
+    private bool illuminatiConfirmed;
+
+    // Use this for initialization
+    void Start()
+    { 
+        illuminatiConfirmed = false;
+        musicSource = GetComponent<AudioSource>();
+        musicSource.PlayDelayed(4.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (GameObject.FindWithTag("TankEnemy") != null)
+        {
+            illuminatiConfirmed = true;
+        }
+        else if (GameObject.FindWithTag("TankEnemy") == null)
+        {
+            illuminatiConfirmed = false;
+        }
+        if (illuminatiConfirmed == true)
+        {
+            musicSource.volume = 0f;
+        }
+        else if (illuminatiConfirmed == false)
+        {
+            musicSource.volume = 0.25f;
+        }
+    }
+}
